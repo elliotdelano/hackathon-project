b7574dbceee604bc33044c00d1c06dc896eacfd5
Brennan Overhoff <brennanoverhoff@gmail.com>
2021-11-06 14:10:30 -0400

    algo

---
+++ algo/chancer.py
@@ -0,0 +1,47 @@
+bonus = .01
+
+class chancer:
+    def __init__(self, school, sat, gpa, ecs, awards):
+        self.school = school
+        self.sat = sat
+        self.gpa = gpa
+        self.ecs = ecs
+        self.awards = awards
+
+    def goat_status(self):
+        #ec/award substring if statements
+        return 0
+
+    def rate_sat(self):
+        #sat rating
+        return 0
+
+    def rate_gpa(self):
+        return 0
+
+    def ecs_bonus(self):
+        return 0
+        #commitment computation
+
+    def awards_bonus(self):
+        return 0
+        #competitiveness bonus
+        #substring search for things like nmsf--non goat awards
+
+
+
+
+class profile:
+    def __init__(self, acceptance_rate, sat_rating, gpa_rating, ecs_sent, ecs_bonus, awards_sent, awards_bonus, goat_rating):
+        self.acceptance_rate  = acceptance_rate
+        self.sat_rating = sat_rating
+        self.gpa_rating = gpa_rating
+        self.ecs_sent = ecs_sent
+        self.ecs_bonus = ecs_bonus
+        self.awards_sent = awards_sent
+        self.awards_bonus = awards_bonus
+        self.goat_rating = goat_rating
+
+    def chance(self):
+        chance = max(self.acceptance_rate * (self.ecs_sent + self.awards_sent) - (self.sat_rating + self.gpa_rating) + bonus * (self.ecs_bonus + self.awards_bonus), self.goat_rating)
+        return min(chance, 1.0)
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask , render_template, session, request, jsonify, redirect, url_for
-#from sentiment import main
+#from sentiment import sentiment
+from algo import chancer
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +10,23 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/chanceme',methods=["GET","POST"])
+def chanceme():
+    if request.method == "POST":
+        school = request.form['school']
+        sat = request.form['sat']
+        gpa = request.form['gpa']
+        ec_0 = request.form['ec_0']
+        ec_1 = request.form['ec_1']
+        ec_2 = request.form['ec_2']
+        ec_3 = request.form['ec_3']
+        ec_4 = request.form['ec_4']
+        ec_5 = request.form['ec_5']
+        ec_6 = request.form['ec_6']
+        ec_7 = request.form['ec_7']
+        ec_8 = request.form['ec_8']
+        ec_9 = request.form['ec_9']
+
+        chancer = chancer.chancer()
+

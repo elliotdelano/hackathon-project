410b8c52eac2fa8145c585668d5339497ac1a98d
Brennan Overhoff <brennanoverhoff@gmail.com>
2021-11-06 13:37:40 -0400

    dumb

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask , render_template, session, request, jsonify, redirect, url_for
-
+#from sentiment import main
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
